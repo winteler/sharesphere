73626733a4584724ea983c45f00d016427e219ee
Nicolas Winteler <winteler@protonmail.com>
2023-09-23 00:05:38 +0200

    Init: start trying to load existing forums to setup client-side checks during forum creation

--- web-app/src/forum.rs
+++ web-app/src/forum.rs
@@ -1,6 +1,9 @@
 use cfg_if::cfg_if;
+use std::collections::{HashSet};
 use leptos::*;
 use leptos_router::{ActionForm, Outlet};
+
+use crate::icons::LoadingIcon;
 
 cfg_if! {
     if #[cfg(feature = "ssr")] {
@@ -39,25 +42,63 @@ pub async fn create_forum(cx: Scope, name: String, description: String, is_nsfw:
     return result;
 }
 
+#[server(GetAllForumNames, "/api")]
+pub async fn get_all_forum_names(cx: Scope) -> Result<HashSet<String>, ServerFnError> {
+    let db_pool = get_db_pool(cx)?;
+    log!("Got db pool");
+
+    let forum_name_vec = sqlx::query!("SELECT name FROM forums").fetch_all(&db_pool).await?;
+
+    let mut forum_name_set: HashSet<String> = HashSet::with_capacity(forum_name_vec.len());
+    for forum_name in forum_name_vec {
+        forum_name_set.insert(forum_name.name);
+    }
+
+    Ok(forum_name_set)
+}
+
 #[component]
 pub fn CreateForum(cx: Scope) -> impl IntoView {
+    use leptos_router::FromFormData;
+
     let create_forum = create_server_action::<CreateForum>(cx);
+    let existing_forums = create_blocking_resource(cx, || (), move |_| get_all_forum_names(cx));
+
+    let (name, set_name) = create_signal(cx, String::default);
+
+    let on_submit = move |event: leptos::ev::SubmitEvent| {
+        let data = CreateForum::from_event(&event);
+        if data.is_err() || data.unwrap().name == "nope!" {
+            // ev.prevent_default() will prevent form submission
+            event.prevent_default();
+        }
+    };
 
     view! { cx,
-        <ActionForm action=create_forum>
-            <div class="flex flex-col gap-1 w-full max-w-md 2xl:max-w-lg max-2xl:mx-auto">
-                <h2 class="p-6 text-4xl max-2xl:text-center">"Create [[forum]]"</h2>
-                <input type="text" name="name" placeholder="[[Forum]] name" class="input input-bordered input-primary"/>
-                <textarea name="description" placeholder="Description" class="textarea textarea-primary h-40"/>
-                <div class="form-control">
-                    <label class="cursor-pointer label">
-                        <span class="label-text">"NSFW content"</span>
-                        <input type="checkbox" name="is_nsfw" class="checkbox checkbox-primary"/>
-                    </label>
-                </div>
-                <button type="submit" class="btn btn-active btn-secondary">"Create"</button>
-            </div>
-        </ActionForm>
+        <Suspense fallback=move || (view! {cx, <LoadingIcon/>})>
+                {
+                    move || {
+                        existing_forums.read(cx).map(|forum_set| {
+                            view! {cx,
+                                <ActionForm action=create_forum on:submit=on_submit>
+                                    <div class="flex flex-col gap-1 w-full max-w-md 2xl:max-w-lg max-2xl:mx-auto">
+                                        <h2 class="p-6 text-4xl max-2xl:text-center">"Create [[forum]]"</h2>
+                                        <input type="text" name="name" placeholder="[[Forum]] name" class="input input-bordered input-primary"/>
+                                        <textarea name="description" placeholder="Description" class="textarea textarea-primary h-40"/>
+                                        <div class="form-control">
+                                            <label class="cursor-pointer label">
+                                                <span class="label-text">"NSFW content"</span>
+                                                <input type="checkbox" name="is_nsfw" class="checkbox checkbox-primary"/>
+                                            </label>
+                                        </div>
+                                        <button type="submit" class="btn btn-active btn-secondary">"Create"</button>
+                                    </div>
+                                </ActionForm>
+                            }
+                        })
+                    }
+                }
+        </Suspense>
     }
 }
 

73bafd6f68dc5fc3097637e70ac592d55c3d88c7
Nicolas Winteler <winteler@protonmail.com>
2024-03-01 21:58:13 +0100

    Init: add new icons, need to select some for sorting

--- web-app/app/src/widget.rs
+++ web-app/app/src/widget.rs
@@ -1,7 +1,7 @@
 use leptos::*;
 
 use crate::constants::{SECONDS_IN_DAY, SECONDS_IN_HOUR, SECONDS_IN_MINUTE, SECONDS_IN_MONTH, SECONDS_IN_YEAR};
-use crate::icons::{AuthorIcon, BoldIcon, ClockIcon, FlameIcon, HotIcon};
+use crate::icons::{AuthorIcon, BoldIcon, ClockIcon, FlameIcon, GraphIcon, HotIcon, HourglassIcon, MedalIcon, MedalIcon2, PodiumIcon, PodiumIcon2, SimpleFlameIcon, TimewatchIcon};
 
 #[component]
 pub fn FormTextEditor(
@@ -112,13 +112,48 @@ pub fn SortWidget() -> impl IntoView {
                 </a>
             </li>
             <li>
+                <a class="tooltip" data-tip="Home">
+                    <SimpleFlameIcon/>
+                </a>
+            </li>
+            <li>
                 <a class="tooltip" data-tip="Details">
                     <FlameIcon/>
                 </a>
             </li>
             <li>
                 <a class="tooltip" data-tip="Stats">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>
+                    <PodiumIcon/>
+                </a>
+            </li>
+            <li>
+                <a class="tooltip" data-tip="Stats">
+                    <PodiumIcon2/>
+                </a>
+            </li>
+            <li>
+                <a class="tooltip" data-tip="Stats">
+                    <MedalIcon/>
+                </a>
+            </li>
+            <li>
+                <a class="tooltip" data-tip="Stats">
+                    <MedalIcon2/>
+                </a>
+            </li>
+            <li>
+                <a class="tooltip" data-tip="Stats">
+                    <GraphIcon/>
+                </a>
+            </li>
+            <li>
+                <a class="tooltip" data-tip="Stats">
+                    <HourglassIcon/>
+                </a>
+            </li>
+            <li>
+                <a class="tooltip" data-tip="Stats">
+                    <TimewatchIcon/>
                 </a>
             </li>
         </ul>

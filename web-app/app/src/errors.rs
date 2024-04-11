3c243212595824c1ebd5f1af96b41fa058ceb80e
Nicolas Winteler <winteler@protonmail.com>
2024-04-11 22:43:53 +0200

    Init: start adding more custom errors

--- web-app/app/src/errors.rs
+++ web-app/app/src/errors.rs
@@ -3,17 +3,20 @@ use thiserror::Error;
 
 #[derive(Clone, Debug, Error)]
 pub enum AppError {
-    #[error("Not Found")]
-    NotFound,
     #[error("Internal Server Error")]
     InternalServerError,
+    #[error("Not Connected")]
+    NotConnected,
+    #[error("Not Found")]
+    NotFound,
 }
 
 impl AppError {
     pub fn status_code(&self) -> StatusCode {
         match self {
-            AppError::NotFound => StatusCode::NOT_FOUND,
             AppError::InternalServerError => StatusCode::INTERNAL_SERVER_ERROR,
+            AppError::NotConnected => StatusCode::FORBIDDEN,
+            AppError::NotFound => StatusCode::NOT_FOUND,
         }
     }
 }

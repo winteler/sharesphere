0601f0981fd6658a6f7a1f4b2314840c7f03e452
Nicolas <winteler@protonmail.com>
2024-06-05 23:20:44 +0200

    Roles: start creating moderation dialog

--- web-app/app/src/forum_management.rs
+++ web-app/app/src/forum_management.rs
@@ -1,5 +1,5 @@
 use const_format::concatcp;
-use leptos::{component, IntoView, Show, SignalGet, use_context, view};
+use leptos::{component, create_rw_signal, IntoView, RwSignal, Show, SignalGet, SignalSet, use_context, view};
 
 use crate::app::UserState;
 use crate::icons::HammerIcon;
@@ -19,28 +19,58 @@ pub fn ForumCockpit() -> impl IntoView {
 
 /// Component to moderate a post
 #[component]
-pub fn ModeratePostButton() -> impl IntoView {
+pub fn ModerateButton(
+    show_dialog: RwSignal<bool>,
+) -> impl IntoView {
     let user_state = use_context::<UserState>();
     view! {
         <Show when=move || match user_state {
             Some(user_state) => user_state.can_moderate.get(),
             None => false,
         }>
-            <button class="btn btn-circle btn-sm btn-ghost"><HammerIcon/></button>
+            <button
+                class="btn btn-circle btn-sm btn-ghost"
+                on:click=move |_| show_dialog.set(true)
+            >
+                <HammerIcon/>
+            </button>
         </Show>
     }
 }
 
-/// Component to moderate a comment
+/// Component to access a post's moderation dialog
 #[component]
-pub fn ModerateCommentButton() -> impl IntoView {
-    let user_state = use_context::<UserState>();
+pub fn ModeratePostButton() -> impl IntoView {
+    let show_dialog = create_rw_signal(false);
     view! {
-        <Show when=move || match user_state {
-            Some(user_state) => user_state.can_moderate.get(),
-            None => false,
-        }>
-            <button class="btn btn-circle btn-sm btn-ghost"><HammerIcon/></button>
-        </Show>
+        <ModerateButton show_dialog/>
     }
 }
+
+/// Component to access a comment's moderation dialog
+#[component]
+pub fn ModerateCommentButton() -> impl IntoView {
+    let show_dialog = create_rw_signal(false);
+    view! {
+        <ModerateButton show_dialog/>
+    }
+}
+
+/// Dialog to moderate a post
+#[component]
+pub fn ModeratePostDialog(
+    show_dialog: RwSignal<bool>,
+) -> impl IntoView {
+    view! {
+    }
+}
+
+/// Dialog to moderate a comment
+#[component]
+pub fn ModerateCommentDialog(
+    show_dialog: RwSignal<bool>,
+) -> impl IntoView {
+    view! {
+    }
+}
+
